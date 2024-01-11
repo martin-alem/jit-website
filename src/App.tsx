0f0cfdf6729862a177259e02d45ac3d3e552ea37
Martin Alemajoh <alemajohmartin@gmail.com>
2024-01-10 21:22:12 -0500

    implement home page and create stubs for about, download, documentation and examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/about/About";
+import Home from "./pages/home/Home";
+import Download from "./pages/downloads/Download";
+import Documentation from "./pages/documentation/Documentation";
+import Example from "./pages/example/Example";
+import NotFound from "./pages/404/404";
 
+function App() {
   return (
-    <>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route index element={<About />} />
+        <Route path="download" element={<Download />} />
+        <Route path="documentation" element={<Documentation />} />
+        <Route path="examples" element={<Example />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/about/About.tsx
@@ -0,0 +1,121 @@
+import { ArrowPathIcon, RocketLaunchIcon, FingerPrintIcon, PuzzlePieceIcon, } from "@heroicons/react/20/solid";
+
+const features = [
+  {
+    name: "High Speed Performance",
+    description: "Experience unparalleled speed in code management with Jit VCS's optimized performance for commit retrieval, branching, and merging.",
+    icon: RocketLaunchIcon,
+  },
+  {
+    name: "Intuitive Branching & Merging",
+    description: "Simplify your development workflow with our user-friendly branching and merging system, designed for both beginners and seasoned professionals",
+    icon: PuzzlePieceIcon,
+  },
+  {
+    name: "Scalability for Large Projects",
+    description: "Jit VCS grows with your project, offering robust scalability options to handle large codebases and extensive development teams.",
+    icon: ArrowPathIcon,
+  },
+  {
+    name: "Streamlined Command Set",
+    description: "Our simplified command set makes version control easy to learn and efficient to use, without compromising on advanced functionalities.",
+    icon: FingerPrintIcon,
+  },
+];
+function About() {
+  return (
+    <>
+      <div className="pt-14">
+        <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+          <div
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#393939] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+        <div className="py-24 sm:py-32 lg:pb-40">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl text-center">
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Jit - Streamlining Your Code, Empowering Collaboration</h1>
+              <p className="mt-6 text-lg leading-8 text-gray-600">
+                Jit VCS is an innovative version control system designed for modern software development needs. With an emphasis on simplicity and efficiency, Jit VCS offers a seamless experience for
+                managing code versions and collaborating on software projects.
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6">
+                <a
+                  href="#"
+                  className="rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600">
+                  Download Jit
+                </a>
+                <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                  Learn more <span aria-hidden="true">→</span>
+                </a>
+              </div>
+            </div>
+            <div className="mt-16 flow-root sm:mt-24">
+              <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                <img
+                  src="https://tailwindui.com/img/component-images/project-app-screenshot.png"
+                  alt="App screenshot"
+                  width={2432}
+                  height={1442}
+                  className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+          <div
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#393939] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+      </div>
+
+      {/* Feature section */}
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:text-center">
+          <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Features and Capabilities</p>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Our system boasts a range of features including fast commit history retrieval, intuitive branching and merging, and robust support for distributed workflows.
+          </p>
+        </div>
+        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+            {features.map((feature) => (
+              <div key={feature.name} className="relative pl-16">
+                <dt className="text-base font-semibold leading-7 text-gray-900">
+                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                    <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                  </div>
+                  {feature.name}
+                </dt>
+                <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+
+      {/* Design Philosophy section */}
+      <div className="mx-auto mt-20 max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:text-center">
+          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Design Philosophy</p>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Our system boasts a range of features including fast commit history retrieval, intuitive branching and merging, and robust support for distributed workflows. We've optimized Jit VCS for
+            speed, ensuring operations like branching and merging are quick and smooth.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default About;
